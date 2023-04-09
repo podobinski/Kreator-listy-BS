89c074a23b1d94b0a9ba7f1cba13cef6c2b93529
podobinski <piotr.podobinski@gmail.com>
2023-04-09 10:25:47 +0200

    czy jest facebook
    Sprawdź czy na stronie jest odnośnik facebook

--- kreator-listy-bs.py
+++ kreator-listy-bs.py
@@ -6,6 +6,20 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+def find_facebook_profile(www_address, driver):
+    try:
+        if not www_address.startswith("http://") and not www_address.startswith("https://"):
+            www_address = "https://" + www_address
+
+        driver.get(www_address)
+        facebook_links = driver.find_elements(By.XPATH, "//a[contains(@href, 'facebook.com')]")
+        if facebook_links:
+            return facebook_links[0].get_attribute('href')
+    except Exception as e:
+        print(f"Error finding Facebook profile for {www_address}: {e}")
+    return None
+
+
 def extract_website_data(url, driver_path):
     service = Service(executable_path=driver_path)
     driver = webdriver.Chrome(service=service)
@@ -24,7 +38,16 @@ def extract_website_data(url, driver_path):
             company_list = driver.find_elements(By.CSS_SELECTOR, "#entityTable dl")
             print(f"Found {len(company_list)} company elements.")
             
-            for company in company_list:
+            main_window_handle = driver.current_window_handle
+
+            for i in range(len(company_list)):
+                companies_on_page = driver.find_elements(By.CSS_SELECTOR, "#entityTable dl")
+                if i >= len(companies_on_page):
+                    print(f"Index {i} is out of range. Skipping this iteration.")
+                    continue
+                
+                company = companies_on_page[i]
+
                 name_element = company.find_element(By.XPATH, "./dd[1]/h3")
                 name = re.sub(r'<span.*?>Nazwa<\/span>', '', name_element.get_attribute("innerHTML")).strip()
                 try:
@@ -34,22 +57,36 @@ def extract_website_data(url, driver_path):
                 except Exception as e:
                     print(f"Error extracting wwwAddress for {name}: {e}")
                     www_address = ""
-                print(f"Extracted name: {name}, wwwAddress: {www_address}")
-                data.append({"Name": name, "WWW Address": www_address})
 
-            # Check if the "next 20" button exists, and click it if it does
+                facebook_url = None
+                if www_address:
+                    new_window_handle = None
+                    try:
+                        driver.execute_script(f"window.open('{www_address}', '_blank')")
+                        new_window_handle = [handle for handle in driver.window_handles if handle != main_window_handle][0]
+                        driver.switch_to.window(new_window_handle)
+                        facebook_url = find_facebook_profile(www_address, driver)
+                    finally:
+                        if new_window_handle:
+                            driver.close()
+                            driver.switch_to.window(main_window_handle)
+                
+                print(f"Extracted name: {name}, wwwAddress: {www_address}, Facebook URL: {facebook_url}")
+                data.append({"Name": name, "WWW Address": www_address, "Facebook URL": facebook_url})
+
             next_page_buttons = driver.find_elements(By.CSS_SELECTOR, "#nextPage")
             if next_page_buttons:
+                print("Clicking the 'next' button...")
                 next_page_button = next_page_buttons[0]
                 
-                # Use JavaScript to click the button
                 driver.execute_script("arguments[0].click();", next_page_button)
 
-                # Wait for the next page to load before proceeding
                 WebDriverWait(driver, 30).until(
                     EC.staleness_of(company_list[-1])
                 )
+                print("Proceeding to the next page...")
             else:
+                print("No more pages found. Exiting loop...")
                 break
 
     except Exception as e:
@@ -58,6 +95,7 @@ def extract_website_data(url, driver_path):
         driver.quit()
 
     return data
+
 
 def save_to_excel(data, output_file):
     df = pd.DataFrame(data)
@@ -69,5 +107,5 @@ if __name__ == "__main__":
     driver_path = "/path/to/chromedriver"  # Replace this with the path to your Chrome WebDriver
     data = extract_website_data(url, driver_path)
     if data:
-        output_file = "output.xlsx"
+        output_file = "bs-lista.xlsx"
         save_to_excel(data, output_file)
