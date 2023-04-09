74a29d2767c1cbd772471865e7fc129dda9118f2
podobinski <piotr.podobinski@gmail.com>
2023-04-09 14:21:26 +0200

    Szukanie Zrzeszenia wg domeny
    Zamiast przeszukiwać treść w poszukiwaniu zrzeszenia sprawdzamy czy są linki do domeny zrzeszenia.

--- affiliation_checker.py
+++ affiliation_checker.py
@@ -1,22 +1,51 @@
 import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
 
+def affiliation_check(url):
+    # Add scheme if it's missing
+    parsed_url = urlparse(url)
+    if not parsed_url.scheme:
+        url = f'http://{url}'
 
-def affiliation_check(website_url):
+    bps_found = False
+    sgb_found = False
+
     try:
-        response = requests.get(website_url)
-        source_code = response.text
+        response = requests.get(url)  # Re-enable SSL verification
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content, 'html.parser')
+            links = soup.find_all('a', href=True)
 
-        bps_present = "BPS" in source_code
-        sgb_present = "SGB" in source_code
+            for link in links:
+                href = link['href']
 
-        if bps_present and sgb_present:
-            return "BPS i SGB"
-        elif bps_present:
-            return "BPS"
-        elif sgb_present:
-            return "SGB"
+                parsed_href = urlparse(href)
+                if parsed_href.netloc.endswith('bankbps.pl'):
+                    bps_found = True
+                elif parsed_href.netloc.endswith('sgb.pl'):
+                    sgb_found = True
+
+                if bps_found and sgb_found:
+                    break
+
         else:
-            return "nie znaleziono"
+            return f"Request failed with status code {response.status_code}"
+    except requests.exceptions.SSLError as e:
+        return f"SSL Error: {str(e)} for URL {url}"
     except Exception as e:
-        print(f"Error checking affiliation for {website_url}: {e}")
+        return f"Error: {str(e)} for URL {url}"
+
+    if bps_found and sgb_found:
+        return "BPS i SGB"
+    elif bps_found:
+        return "BPS"
+    elif sgb_found:
+        return "SGB"
+    else:
         return "nie znaleziono"
+
+# Example usage
+url = "https://example.com"
+result = affiliation_check(url)
+print(result)
