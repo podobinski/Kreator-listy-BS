08feefaaa07cdf6889a25648abc617af6613b769
podobinski <piotr.podobinski@gmail.com>
2023-04-09 10:23:59 +0200

    Revert "Sprawdź na www jest odnośnik Facebook"
    This reverts commit bfe92325f46f2987911b08a1b38eafa73496f45a.

--- kreator-listy-bs.py
+++ kreator-listy-bs.py
@@ -6,16 +6,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-def find_facebook_profile(www_address, driver):
-    try:
-        driver.get(www_address)
-        facebook_links = driver.find_elements(By.XPATH, "//a[contains(@href, 'facebook.com')]")
-        if facebook_links:
-            return facebook_links[0].get_attribute('href')
-    except Exception as e:
-        print(f"Error finding Facebook profile for {www_address}: {e}")
-    return None
-
 def extract_website_data(url, driver_path):
     service = Service(executable_path=driver_path)
     driver = webdriver.Chrome(service=service)
@@ -34,14 +24,7 @@ def extract_website_data(url, driver_path):
             company_list = driver.find_elements(By.CSS_SELECTOR, "#entityTable dl")
             print(f"Found {len(company_list)} company elements.")
             
-            for i in range(len(company_list)):
-                companies_on_page = driver.find_elements(By.CSS_SELECTOR, "#entityTable dl")
-                if i >= len(companies_on_page):
-                    print(f"Index {i} is out of range. Skipping this iteration.")
-                    continue
-                
-                company = companies_on_page[i]
-
+            for company in company_list:
                 name_element = company.find_element(By.XPATH, "./dd[1]/h3")
                 name = re.sub(r'<span.*?>Nazwa<\/span>', '', name_element.get_attribute("innerHTML")).strip()
                 try:
@@ -51,24 +34,22 @@ def extract_website_data(url, driver_path):
                 except Exception as e:
                     print(f"Error extracting wwwAddress for {name}: {e}")
                     www_address = ""
+                print(f"Extracted name: {name}, wwwAddress: {www_address}")
+                data.append({"Name": name, "WWW Address": www_address})
 
-                facebook_url = find_facebook_profile(www_address, driver) if www_address else None
-                print(f"Extracted name: {name}, wwwAddress: {www_address}, Facebook URL: {facebook_url}")
-                data.append({"Name": name, "WWW Address": www_address, "Facebook URL": facebook_url})
-
+            # Check if the "next 20" button exists, and click it if it does
             next_page_buttons = driver.find_elements(By.CSS_SELECTOR, "#nextPage")
             if next_page_buttons:
-                print("Clicking the 'next' button...")
                 next_page_button = next_page_buttons[0]
                 
+                # Use JavaScript to click the button
                 driver.execute_script("arguments[0].click();", next_page_button)
 
+                # Wait for the next page to load before proceeding
                 WebDriverWait(driver, 30).until(
                     EC.staleness_of(company_list[-1])
                 )
-                print("Proceeding to the next page...")
             else:
-                print("No more pages found. Exiting loop...")
                 break
 
     except Exception as e:
@@ -88,5 +69,5 @@ if __name__ == "__main__":
     driver_path = "/path/to/chromedriver"  # Replace this with the path to your Chrome WebDriver
     data = extract_website_data(url, driver_path)
     if data:
-        output_file = "bs-lista.xlsx"
+        output_file = "output.xlsx"
         save_to_excel(data, output_file)
